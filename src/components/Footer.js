7ff307f266baea78a23ef2f5647eb52cb20e4478
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-08-29 16:48:39 -0400

    Deleted App.css and created custom style.css file.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,24 @@
 import React from "react";
 
+import '../style.css';
+
 
 function Footer() {
     return (
-        <footer>
-            <div className="grid-container">
-                <div className="grid-x padding-x footer-container">
-                    <div className="cell large-4 medium-4 small-4">
+        
+            <footer className="grid-container full">
+                <section className="grid-x padding-x footer">
+                    <article className="cell large-4 medium-4 small-4">
                         <p>thing 1</p>
-                    </div>
-                    <div className="cell large-4 medium-4 small-4">
+                    </article>
+                    <article className="cell large-4 medium-4 small-4">
                         <p>thing 2</p>
-                    </div>
-                    <div className="cell large-4 medium-4 small-4">
+                    </article>
+                    <article className="cell large-4 medium-4 small-4">
                         <p>thing 3</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+                    </article>
+                </section>
+            </footer>
         
     );
 }

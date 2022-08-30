66bf75e5aed33817079a23e8c43744479304dde0
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-08-30 19:32:51 -0400

    Finished wireframe. Ready for content.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import '../style.css';
+
 function About() {
     return (
-        <section className="grid-container">
+        <section className="grid-container about-container">
             <h1>About page</h1>
+            <section className="grid-x grid-margin-x align-center">
+                <article className="cell large-5 medium-5 small-5 gray">left</article>
+                <article className="cell large-5 medium-5 small-5 gray">right</article>
+            </section>
         </section>
     );
 }
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -4,6 +4,62 @@ function Work() {
     return (
         <section className="grid-container">
             <h1>Work page</h1>
+            <section className="grid-x grid-padding-x align-center">
+            <article className="card large-4 medium-6 small-12">
+                <div className="card-divider">
+                    <h4>I'm featured</h4>
+                </div>
+                {/* <img src="assets/img/generic/rectangle-1.jpg" /> */}
+                <div className="card-section">
+                    <p>This card makes use of the card-divider element.</p>
+                </div>
+            </article>
+            <article className="card large-4 medium-6 small-12">
+                <div className="card-divider">
+                    <h4>I'm featured</h4>
+                </div>
+                {/* <img src="assets/img/generic/rectangle-1.jpg" /> */}
+                <div className="card-section">
+                    <p>This card makes use of the card-divider element.</p>
+                </div>
+            </article>
+            <article className="card large-4 medium-6 small-12">
+                <div className="card-divider">
+                    <h4>I'm featured</h4>
+                </div>
+                {/* <img src="assets/img/generic/rectangle-1.jpg" /> */}
+                <div className="card-section">
+                    <p>This card makes use of the card-divider element.</p>
+                </div>
+            </article>
+            <article className="card large-4 medium-6 small-12">
+                <div className="card-divider">
+                    <h4>I'm featured</h4>
+                </div>
+                {/* <img src="assets/img/generic/rectangle-1.jpg" /> */}
+                <div className="card-section">
+                    <p>This card makes use of the card-divider element.</p>
+                </div>
+            </article>
+            <article className="card large-4 medium-6 small-12">
+                <div className="card-divider">
+                    <h4>I'm featured</h4>
+                </div>
+                {/* <img src="assets/img/generic/rectangle-1.jpg" /> */}
+                <div className="card-section">
+                    <p>This card makes use of the card-divider element.</p>
+                </div>
+            </article>
+            <article className="card large-4 medium-6 small-12">
+                <div className="card-divider">
+                    <h4>I'm featured</h4>
+                </div>
+                {/* <img src="assets/img/generic/rectangle-1.jpg" /> */}
+                <div className="card-section">
+                    <p>This card makes use of the card-divider element.</p>
+                </div>
+            </article>
+            </section>
         </section>
     );
 }

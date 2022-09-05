5858cdf1713d0a55227c6a41e0de71284172622c
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-09-05 14:54:58 -0400

    Added the rest of for the work page and styled.

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,41 +15,43 @@ const Header = () => {
     });
 
     return (
-        <nav className="grid-container full">
-        <section className="grid-x header-nav">
-            
-            <article className="cell large-1 medium-1 small-1">
-            </article>
+        <header>
+            <nav className="grid-container full">
+            <section className="grid-x header-nav">
+                
+                <article className="cell large-1 medium-1 small-1">
+                </article>
 
-            <article className="cell large-1 medium-1 small-1" ref={logo}>
-                <Link to="/">
-                    <img
-                        className="dl-dev-logo"
-                        src={require("../images/dl-dev-logo-blue.png")}
-                        alt="Daniel Lundy Development Logo"
-                    />
-                </Link>
-            </article>
+                <article className="cell large-1 medium-1 small-1" ref={logo}>
+                    <Link to="/">
+                        <img
+                            className="dl-dev-logo"
+                            src={require("../images/dl-dev-logo-blue.png")}
+                            alt="Daniel Lundy Development Logo"
+                        />
+                    </Link>
+                </article>
 
-            <article className="cell large-8 medium-6 small-6">
-            </article>
+                <article className="cell large-8 medium-6 small-6">
+                </article>
 
-            <article className="cell large-2 medium-2 small-2 align-self-bottom">
-                <ul className="menu align-right">
-                    <li> 
-                        <Link to="/about">
-                            <button type="button" className="submit button">About</button>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/work">
-                            <button type="button" className="submit button">Work</button>
-                        </Link>
-                    </li>
-                </ul>
-            </article>
-        </section>
-        </nav>
+                <article className="cell large-2 medium-2 small-2 align-self-bottom">
+                    <ul className="menu align-right">
+                        <li> 
+                            <Link to="/about">
+                                <button type="button" className="submit button">About</button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/work">
+                                <button type="button" className="submit button">Work</button>
+                            </Link>
+                        </li>
+                    </ul>
+                </article>
+            </section>
+            </nav>
+        </header>
     );
 };
 

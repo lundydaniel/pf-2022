7db0926562123346a994c0854e9c444b78e23ecd
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-09-16 10:44:44 -0400

    Renamed projects.json to project.js.

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,41 +1,72 @@
 import React from "react";
-// import Slider from "react-slick";
+import Slider from "react-slick";
+import { projects } from '../components/projects';
 
-import Projects from '../projects.json';
-// import SimpleSlider from "./test";
 import '../style.css';
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function Work() {
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
+            }
+            ]
+        };
 
     return (
-        <section className="grid-container main-container">
-            <article className="grid-x grid-padding-x align-center work-section">
-            {
-                // looping through json to display
-                Projects && Projects.map( projects => {
-                    return(
-                        
-                            <article className="card large-5 medium-6 small-12" key={ projects.id }>
-                            <div className="card-divider">
-                                <h4>{ projects.title }</h4>
-                            </div>
-                            <img
-                                src={ projects.img }
-                                alt={ projects.alt } 
-                            />
-                            <div className="card-section">
-                                <p>{ projects.description }</p>
-                            </div>
-                            <div className="card-divider">
-                                <a href={ projects.link } className="button" target="_blank" rel="noopener noreferrer">View Project</a>
-                            </div>
-                            </article>
-                    )
-                })
-            }
-            </article>
+        <section className="main-work-container">
+                <Slider {...settings}>
+                    <article className="work-section">
+                            {
+                            // looping through Project Object to display data
+                            projects && projects.map(( item ) => ( 
+                                <article className="work-card" key={ item.id }>
+                                <div className="card-divider-top">
+                                    <h4>{ item.title }</h4>
+                                </div>
+                                <img
+                                    src={ item.img }
+                                    alt={ item.alt } 
+                                />
+                                <div className="card-article">
+                                    <p>{ item.description }</p>
+                                </div>
+                                <div className="card-divider-bottom">
+                                    <a href={ item.link } className="button" target="_blank" rel="noopener noreferrer">View Project</a>
+                                </div>
+                                </article>
+                            ))}
+                    </article>
+                </Slider>
         </section>
     )
 }
---
+++ src/projects.js
@@ -0,0 +1,52 @@
+    {
+        "projects" [
+            {
+                "id": 1,
+                "title": "Mad Movies",
+                "img": "json_imgs/mad-movies.png",
+                "alt": "Movie Information Database website.",
+                "description": "The idea with this site was to create a functional movie database for users find information on their favorite movies. I used HTML, CSS, Javascript, JSON, XHR, and PHP coupled with MySQL to create a dynamic website. I also used Foundations CSS and sammy.js to help with the UI/UX of the site.",
+                "link": "https://daniellundydev.ca/madmovies"
+            },
+            {
+                "id": 2,
+                "title": "Easy Grocery",
+                "img": "json_imgs/easygrocery.png",
+                "alt": "Ecommerce Grocery Store Site.",
+                "description": "The idea with this site was to create a mock grocery store that would utilize PHP, JS, JQuery, XHR, JSON, HTML, CSS and MySQL to display data. The results give a fully functional dynamic ecommerce site.",
+                "link": "https://easygroceries.daniellundydev.ca/#/home/"
+            },
+            {
+                "id": 3,
+                "title": "Internet Of Things",
+                "img": "json_imgs/iot.png",
+                "alt": "Internet of Things Project.",
+                "description": "This was a fun exploratory project using a BBC Micro:bit. The object was to create an interactive web based advertisement for a vacation destination. The tech stack for this was HTML, CSS and Javascript.",
+                "link": "https://daniellundydev.ca/iot"
+            },
+            {
+                "id": 4,
+                "title": "The Loopy Prospector",
+                "img": "json_imgs/prospector.png",
+                "alt": "A Javacript Game.",
+                "description": "Welcome to The Loopy Propector. This is a Javascript game done with current ES6 practices. Also, utilizing CSS and HTML. The object is to see how much gold you can get before the river runs dry!",
+                "link": "https://daniellundydev.ca/loopy_prospector"
+            },
+            {
+                "id": 5,
+                "title": "Task Today",
+                "img": "json_imgs/task-today.png",
+                "alt": "Vue.js Web Application.",
+                "description": "For this project I decided to tackle a simple Task taker web application. To accomplish this I chose to use Vue.js. For styling and content I used CSS and HTML and created a custom background image. One feature of the project I added was json data to give the user and inspirational qoute when using the web app.",
+                "link": "https://daniellundydev.ca/task_today"
+            },
+            {
+                "id": 6,
+                "title": "Bball Connection",
+                "img": "json_imgs/bballcon.png",
+                "alt": "Bball Connection Wordpress Ecommerce Site.",
+                "description": "This site is a custom Wordpress Ecommerce site where I utilized _underscores to help create a custom child theme. I also implemented some custom functionality to the WooCommerce plugin.",
+                "link": "https://staging.daniellundydev.ca/bballconnection/"
+            }
+        ]
+    }

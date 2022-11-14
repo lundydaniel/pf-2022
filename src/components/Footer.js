3ac5d1380bddf887915fa77f13385bc0fda35209
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-11-14 12:12:52 -0400

    Added content to the footer and finished it off. Ready for mobile friendliness. Added Font Awesome.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,56 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faInstagram, faFacebook,  } from "@fortawesome/free-brands-svg-icons";
 
 import '../style.css';
 
 
 function Footer() {
+    
     return (
 
         //*! Add gravatar for email footer - look at security issues if any
         
             <footer className="grid-container full">
-                <section className="grid-x padding-x footer">
-                    <article className="cell large-4 medium-4 small-4">
-                        <p>thing 1</p>
+                <section className="grid-x footer">
+                    <article className="cell large-1 medium-1 small-1">
                     </article>
-                    <article className="cell large-4 medium-4 small-4">
-                        <p>thing 2</p>
+                    <article className="cell large-3 medium-3 small-3">
+                    <ul className="menu simple social-media">
+                            <li>
+                                <a href="https://www.facebook.com/dan.lundy.31">
+                                    <FontAwesomeIcon icon={faFacebook} />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/0nthedl21/">
+                                    <FontAwesomeIcon icon={faInstagram} />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/0nthedl_">
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </a>
+                            </li>
+                        </ul>
                     </article>
-                    <article className="cell large-4 medium-4 small-4">
-                        <p>thing 3</p>
+                    <article className="cell large-1 medium-1 small-1">
+                    </article>
+                    <article className="cell large-3 medium-3 small-3">
+                        <p>&copy; Daniel Lundy Development</p>
+                    </article>
+                    <article className="cell large-1 medium-1 small-1">
+                    </article>
+                    <article className="cell large-3 medium-3 small-3 email-link">
+                        <ul className="menu simple email-link">
+                            <li>Feel free to reach out here.</li>
+                            <li>
+                                <a href="mailto:admin@daniellundydev.ca">
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                </a>
+                            </li>
+                        </ul>
                     </article>
                 </section>
             </footer>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,6 +15,7 @@ function Home() {
     const mainContainer = useRef();
 
     //Creating a custom ease for the CTA in the about section
+    gsap.registerPlugin(CustomEase);
     const workIcon = useRef();
     CustomEase.create("workIconEase", "M0,0.005 C0,0.005 0.056,0.445 0.175,0.445 0.294,0.445 0.332,0 0.332,0 0.332,0 0.414,1 0.671,1 0.991,1 1,0 1,0");
 
